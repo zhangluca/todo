1c8f000290266080e052b32e2be1487b678e77eb
zhangluca <youcandoitttttt@gmail.com>
2020-07-29 12:17:46 -0400

    Updates

---
+++ precache-manifest.d2a9efffd48f2241c9ad5528f0c231d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724ccd05e4d0a90f3bbb5b3121b16d14",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "d0f4610b4bb0cdc64ee3",
+    "url": "/todo/static/css/main.b9fffac8.chunk.css"
+  },
+  {
+    "revision": "6a84c7da698c8cf8a3b7",
+    "url": "/todo/static/js/2.4510165c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.4510165c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f4610b4bb0cdc64ee3",
+    "url": "/todo/static/js/main.84cd17ad.chunk.js"
+  },
+  {
+    "revision": "4be3134114a828ee75b2",
+    "url": "/todo/static/js/runtime-main.fd899b97.js"
+  }
+]);
